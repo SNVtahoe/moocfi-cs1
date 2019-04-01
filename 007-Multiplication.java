5af33ae736b882c0541d5ff74921879db178c1d5
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-03-31 21:43:58 -0700

    Exercise 007 - James Bond.

---
+++ 007-Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+  public static void main(String[] args) {
+    int x = 2;
+    int y = 8;
+    int total = x * y;
+    System.out.println(x + " * " + y + " = " + total);
+  }
+}
