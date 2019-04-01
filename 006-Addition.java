54b63fd382845ebdd8137014c4999e25b4bbf23a
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-03-31 21:36:05 -0700

    Exercise 006 - Addition.

---
+++ 006-Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 4;
+    int total = x + y;
+    System.out.println(x + " + " + y + " = " + total);
+  }
+}
