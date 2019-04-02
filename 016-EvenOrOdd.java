9db9723b9d177dc5a483eb5683ce3a5e8656463d
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-02 10:53:07 -0700

    Exercise 016 - EvenOrOdd.

---
+++ 016-EvenOrOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Type a number: ");
+    int x = Integer.parseInt(reader.nextLine());
+
+    if (x % 2 == 0) {
+      System.out.println("Number " + x + " is even.");
+    } else {
+      System.out.println("Number " + x + " is odd.");
+    }
+
+    reader.close();
+  }
+}
