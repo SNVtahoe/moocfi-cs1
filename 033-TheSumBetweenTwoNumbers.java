1b7f4570ce606246bbee3c859b6d97f873da141b
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-03 11:47:59 -0700

    Exercise 033 - TheSumBetweenTwoNumbers.

---
+++ 033-TheSumBetweenTwoNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TheSumBetweenTwoNumbers {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    System.out.print("First: ");
+    int lower = Integer.parseInt(reader.nextLine());
+    System.out.print("Last: ");
+    int upper = Integer.parseInt(reader.nextLine());
+
+    int sum = 0;
+
+    while (lower <= upper) {
+      sum += lower;
+      lower++;
+    }
+
+    System.out.println("The sum is " + sum);
+
+    reader.close();
+  }
+}
