138115fe07f6d312fee24863a2b995a7f5c23107
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-04 15:06:01 -0700

    Exercise 072.2 - Your First Money Transfer.

--- 072-Accounts.java
+++ 072-Accounts.java
@@ -1,7 +1,17 @@
 public class Accounts {
   public static void main(String[] args) {
-    Account myAccount = new Account("Kimi's ice-cream fund", 100.0);
-    myAccount.deposit(20.0);
-    System.out.println(myAccount);
+    /*
+     * Exercise 72.1 Account myAccount = new Account("Kimi's ice-cream fund",
+     * 100.0); myAccount.deposit(20.0); System.out.println(myAccount);
+     */
+
+    Account matt = new Account("Matt's account", 1000.0);
+    Account kimi = new Account("My account", 0.0);
+
+    matt.withdrawal(100);
+    kimi.deposit(100);
+
+    System.out.println(matt);
+    System.out.println(kimi);
   }
 }
