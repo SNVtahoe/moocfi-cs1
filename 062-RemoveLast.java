b75dbf33ac5cce4c7d83e41016242272ca85716a
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-03 19:05:15 -0700

    Exercise 062 - RemoveLast.

---
+++ 062-RemoveLast.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class RemoveLast {
+  public static void main(String[] args) {
+    ArrayList<String> brothers = new ArrayList<String>();
+    brothers.add("Dick");
+    brothers.add("Henry");
+    brothers.add("Michael");
+    brothers.add("Bob");
+
+    System.out.println("brothers: ");
+    System.out.println(brothers);
+
+    removeLast(brothers);
+
+    System.out.println(brothers);
+  }
+
+  public static void removeLast(ArrayList<String> list) {
+    int toRemove = list.size() - 1;
+    list.remove(toRemove);
+  }
+}
