5c11a06c1cb4eb8ebce37f0e61eae89b1798091a
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-03 18:55:54 -0700

    Exercise 046 - AverageOfGivenNumbers.

---
+++ 046-AverageOfGivenNumbers.java
@@ -0,0 +1,14 @@
+public class AverageOfGivenNumbers {
+  public static void main(String[] args) {
+    double answer = average(4, 3, 6, 1);
+    System.out.println("average: " + answer);
+  }
+
+  public static int sum(int num1, int num2, int num3, int num4) {
+    return num1 + num2 + num3 + num4;
+  }
+
+  public static double average(int num1, int num2, int num3, int num4) {
+    return sum(num1, num2, num3, num4) / (double) 4;
+  }
+}
