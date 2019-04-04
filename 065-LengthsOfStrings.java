31f5cafe10c36d3cb37ed7c6b982d352bab38053
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-03 19:06:46 -0700

    Exercise 065 - LengthsOfStrings.

---
+++ 065-LengthsOfStrings.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class LengthsOfStrings {
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Hallo");
+    list.add("Moi");
+    list.add("Benvenuto!");
+    list.add("badger badger badger badger");
+    ArrayList<Integer> lengths = lengths(list);
+
+    System.out.println("The lengths of the Strings: " + lengths);
+  }
+
+  public static ArrayList<Integer> lengths(ArrayList<String> list) {
+    ArrayList<Integer> stringLengths = new ArrayList<Integer>();
+
+    for (String phrase : list) {
+      stringLengths.add(phrase.length());
+    }
+
+    return stringLengths;
+  }
+}
