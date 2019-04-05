610c2401bf69077c9eb979af0549527eb0807ad5
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-05 11:11:09 -0700

    Exercise 080 - Dice.

---
+++ 080-Dice.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    Dice dice = new Dice(6);
+
+    int i = 0;
+
+    while (i < 10) {
+      System.out.println(dice.roll());
+      i++;
+    }
+  }
+}
+
+/*
+ * import java.util.Random;
+ * 
+ * public class Dice { private Random random; private int numberOfSides;
+ * 
+ * public Dice(int numberOfSides) { this.numberOfSides = numberOfSides; random =
+ * new Random(); }
+ * 
+ * public int roll() { return random.nextInt(numberOfSides) + 1; } }
+ */
